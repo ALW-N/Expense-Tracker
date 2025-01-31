da764a5175b5c433cfb2ca2fda299c270604f2b6
ALW-N <officialalwintomy@gmail.com>
2025-01-31 14:15:39 +0530

    Commit-2.1(Fronntend_shadcn)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
 
-const App = () => {
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
   return (
-    <div className='text-center '>App</div>
+    <>
+      <h1 className='text-2xl text-center'>Hello There!</h1>
+      <Button>Click me</Button>
+    </>
   )
 }
 
